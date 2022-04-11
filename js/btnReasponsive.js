2c2cd7bd2163a63ac1c39609669436def8fce472
mi nombre <desarrollowebaps@gmail.com>
2022-04-10 20:25:02 -0400

    PROYECTO NUEVO

---
+++ js/btnReasponsive.js
@@ -0,0 +1,9 @@
+let contHeader_navbar_ul = document.querySelector("#contHeader_navbar_ul")
+let btnResponsive = document.querySelector("#btnResponsive")
+
+
+
+btnResponsive.addEventListener("click", ()=>{
+        contHeader_navbar_ul.classList.toggle("contHeader_navbar_ul_JS")
+    
+})
